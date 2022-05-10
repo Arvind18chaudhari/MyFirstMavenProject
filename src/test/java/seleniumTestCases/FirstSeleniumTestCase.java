72e47c58f714f51a2a31af6d502ded7f736758b9
Pradnyamali <pradnyamali12@gmail.com>
2022-05-10 19:50:48 +0530

    adding selenium tests

---
+++ src/test/java/seleniumTestCases/FirstSeleniumTestCase.java
@@ -0,0 +1,39 @@
+package seleniumTestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class FirstSeleniumTestCase {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        System.out.println("********** initiating selenium test case **********");
+        System.setProperty("webdriver.chrome.driver","./src/test/resources/Drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().fullscreen();
+        driver.get("https://www.google.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void testSearchGoogle(){
+        driver.findElement(By.name("q")).sendKeys("Selenium Testing");
+        driver.findElement(By.name("q")).submit();
+        String actual = driver.getTitle();
+        Assert.assertEquals(actual,"selenium testing - Google Search");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+        System.out.println("********** Exiting selenium test **********");
+    }
+}
